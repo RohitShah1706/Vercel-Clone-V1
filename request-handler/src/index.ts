30fa05a450194858b4f201566553052bbc96ae03
RohitShah1706 <rlshah03@gmail.com>
2024-02-13 15:14:23 +0530

    added 404 page not found to request handler service

--- request-handler/src/index.ts
+++ request-handler/src/index.ts
@@ -5,6 +5,25 @@ import {getFileType} from "./file";
 
 const app = express();
 
+app.get("/404/*", async (req, res) => {
+	console.log("hit 404");
+	const filePath = (req.params as {[key: string]: string})["0"];
+	console.log("filePath", filePath);
+	const Key = `404/${filePath}`;
+
+	const contents = await getObject(Key);
+	if (contents === null) {
+		res.status(404).send("Not found");
+		return;
+	}
+
+	const type = getFileType(filePath);
+
+	// ! do this so in browser we don't download file when we request but instead tell browser to render it
+	res.setHeader("Content-Type", type);
+	res.send(contents);
+});
+
 app.get("/:id/*", async (req, res) => {
 	// ! NOTE: idea here it to capture "id" of build from subdomain
 	// Example: `https://<id>.vercel-clone.com` - so host will be `<id>.vercel-clone.com`
@@ -20,16 +39,25 @@ app.get("/:id/*", async (req, res) => {
 	const Key = `dist/${id}/${filePath}`;
 
 	const contents = await getObject(Key);
+	const type = getFileType(filePath);
+
 	if (contents === null) {
-		res.status(404).send("File not found");
+		if (type === "text/html") {
+			res.redirect("/404/index.html");
+			return;
+		}
+
+		res.status(404).send("Not found");
 		return;
 	}
-
-	const type = getFileType(filePath);
 
 	// ! do this so in browser we don't download file when we request but instead tell browser to render it
 	res.setHeader("Content-Type", type);
 	res.send(contents);
+});
+
+app.get("/*", (req, res) => {
+	res.redirect("/404/index.html");
 });
 
 const PORT = 8080;
